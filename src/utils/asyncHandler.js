676cff4959e4a77592e85d91a254ef7efa36b6f4
Harshit Pant <harshitpant2005@gmail.com>
2026-05-11 20:07:11 +0530

    asynchandler

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,19 @@
+const asyncfHandler = async () =>{
+}
+
+export default asyncHandler;
+
+
+// const asyncHandler = (fn) => async (req,res,next) => {
+//     try{
+//         await fn(req,res,next)
+
+//     }
+//     catch(err){
+//         res.status(err.code || 500).json({
+//             success:false,
+//             message:err.message
+//         })
+//     }
+
+// }
