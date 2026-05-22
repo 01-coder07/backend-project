dbb68aa33b5e91c38008108bcbef685868459103
Harshit Pant <harshitpant2005@gmail.com>
2026-05-22 15:54:58 +0530

    login logic

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -5,7 +5,26 @@ import {uploadOnCloudinary} from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 
 
+const generateAccessAndRefreshToken = async (userId) => {
+  try {
+    const user = await User.findById(userId)
+    const accessToken = user.generateAccessToken()
+    const refreshToken = user.generateRefreshToken()
+
+    user.refreshToken = refreshToken;
+    await user.save({validateBeforeSave:false})
+
+    return {accessToken,refreshToken}
+
+  } catch (error) {
+    throw new ApiError(500,"Something went wrong while generating access and refresh token")
+    
+  }
+}
+
+
 const registerUser = asyncHandler(async (req,res) => {
+
 
    // get user details from frontend
    // validation  - not empty
@@ -31,7 +50,8 @@ const registerUser = asyncHandler(async (req,res) => {
 
    if(existedUser) throw new ApiError(401,"User already exist!");
 
-   const avatarLocalPath = req.files?.avatar[0]?.path
+   const avatarLocalPath = req.files?.avatar?.[0]?.path
+
    const coverImageLocalPath = req.files?.coverImage?.[0]?.path
 
    if(!avatarLocalPath)  throw new ApiError(400,'Avatar file is requried')
@@ -64,4 +84,52 @@ const registerUser = asyncHandler(async (req,res) => {
 
 })
 
-export {registerUser};
+const loginUser = asyncHandler(async (req,res) => {
+   
+    // get email & password from frontend / postman
+    // valid email - means check if this user email exist in database or not
+    // check password : if wrong , dont proceed further. 
+    // generate accesstoken , generate refreshtoken
+    // save refrestoken in db
+    // send response
+
+    const {email , username , password} = req.body
+    if(!username || !email)return new ApiError(402,"username or email is required");
+
+    const user =  await User.findOne({
+        $or:[{username} , {email}]
+    })
+
+   if(!user) return new ApiError(404,"Please register first");
+   
+   const isPasswordValid = await user.isPasswordCorrect(password)
+   if(!isPasswordValid) return new ApiError(404,'Invalid Password');
+
+   const {accessToken , refreshToken } = await generateAccessAndRefreshToken(user.__id);
+
+   const options = {   // Because default cookies can be modified by frontend , so doing this stops it tho
+    httpOnly:true,
+    secure:true,
+   }
+
+   return res
+   .status(200)
+   .cookie("accessToken",accessToken,options)
+   .cookie("refreshToken",refreshToken,options)
+   .json(
+    new ApiResponse(
+        200,
+        "User Logged in Successfully"
+    )
+   )
+
+})
+
+const logoutUser = asyncHandler(async (req,res) => {
+    
+})
+
+export {
+    registerUser,
+    loginUser,
+};
