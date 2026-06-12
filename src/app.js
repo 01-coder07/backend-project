f26359d0ab95a28108ca472f89532577ceca8a63
Harshit Pant <harshitpant2005@gmail.com>
2026-06-12 10:36:14 +0530

    add like model and routes: implement schema and initial route setup for likes

--- src/app.js
+++ src/app.js
@@ -11,14 +11,15 @@ app.use(cors({
 }))
 
 app.use(express.json({limit:"16kb"}))
-app.use(express.urlencoded({extended:false}))
+app.use(express.urlencoded({extended:true}))
 app.use(express.static('public'))
 app.use(cookieParser())
 
 // separtion of routes from abovee middlewares
 import userRoutes from './routes/user.routes.js';
 
-app.use('/api/v1/users',userRoutes) 
+app.use('/api/v1/users',userRoutes)
+
 
 // http://localhost:8000/api/v1/users/register
 
